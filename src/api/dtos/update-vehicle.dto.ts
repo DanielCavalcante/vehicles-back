3a2074eb19e9783253567be4da6b80cecd694f9b
Daniel Cavalcante de Souza <danielcdesouza@gmail.com>
2024-11-12 17:40:38 -0300

    Add swagger open api

--- src/api/dtos/update-vehicle.dto.ts
+++ src/api/dtos/update-vehicle.dto.ts
@@ -1,4 +1,4 @@
-import { PickType } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 import { VehicleDTO } from './vehicle.dto';
 
@@ -7,6 +7,7 @@ export class UpdateVehicleDTO extends PickType(VehicleDTO,
 
   @IsNotEmpty({ message: "O id precisa ser informado." })
   @IsNumber()
+  @ApiProperty()
   id: number;
 
 }
--- src/api/dtos/vehicle.dto.ts
+++ src/api/dtos/vehicle.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 const PLACA_REGEX = /^[A-Z]{3}\d{1}[A-Z]{1}\d{2}$/;
@@ -5,29 +6,35 @@ const CHASSI_REGEX = /^[A-HJ-NPR-Z0-9]{17}$/;
 const RENAVAM_REGEX = /^\d{11}$/;
 
 export class VehicleDTO {
+  @ApiProperty()
   @IsNotEmpty({ message: "Placa precisa ser informada." })
   @IsString()
   @Matches(PLACA_REGEX)
   placa: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: "Chassi precisa ser informada." })
   @IsString()
   @Matches(CHASSI_REGEX)
   chassi: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: "Renavam precisa ser informada." })
   @IsString()
   @Matches(RENAVAM_REGEX)
   renavam: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: "Modelo precisa ser informado." })
   @IsString()
   modelo: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: "Marca precisa ser informada." })
   @IsString()
   marca: string;
 
+  @ApiProperty()
   @IsString()
   ano: string;
 }
